73a05ad5941c3705d4dc9d02e58cdc7e2614c780
vfuller1 <victor.g.fuller@gmail.com>
2026-02-12 09:29:55 -0600

    add sql identity

--- aks.tf
+++ aks.tf
@@ -1,4 +1,11 @@
-# aks.tf
+# 1. Create the User-Assigned Identity
+resource "azurerm_user_assigned_identity" "aviator_identity" {
+  name                = "id-aviator-core-prod"
+  location            = azurerm_resource_group.aviator.location
+  resource_group_name = azurerm_resource_group.aviator.name
+}
+
+# 2. Configure the AKS Cluster
 resource "azurerm_kubernetes_cluster" "aviator_core" {
   name                = "aks-aviator-core"
   location            = azurerm_resource_group.aviator.location
@@ -6,14 +13,16 @@ resource "azurerm_kubernetes_cluster" "aviator_core" {
   dns_prefix          = "aviatorcore"
 
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2as_v6" # Updated to supported East US 2 SKU
+    name         = "default"
+    node_count   = 1
+    vm_size      = "Standard_D2as_v6" 
     os_disk_type = "Managed"
   }
 
+  # Updated from SystemAssigned to UserAssigned
   identity {
-    type = "SystemAssigned"
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aviator_identity.id]
   }
 
   tags = {
